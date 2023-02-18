133bcb69530ab54b2cfa764562c24d4ba8b21515
chrisstruong <chrisstruong@gmail.com>
2023-02-17 19:10:58 -0500

    update live link

--- src/components/CapstoneDetail.jsx
+++ src/components/CapstoneDetail.jsx
@@ -4,7 +4,7 @@ function Capstone (props) {
         <div className='projectDetails'>
             <p>TECH: React.js | Express | JSX | MongoDB | Mongoose | JavaScript | JWT | Passport | HTML5 | CSS3 | Flexbox</p>
             <div className="projects-href">
-            {/* <a href="https://sei-1031.netlify.app/" target="_blank" className='href'>Live Link</a> */}
+            <a href="https://sei-1031.netlify.app/" target="_blank" className='href'>Live Link</a>
             <a href="https://github.com/Chrisstruong/SEI1031_chatapp_server" className='href' target="_blank">Github Repository (server)</a>
             <a href="https://github.com/Chrisstruong/SEI1031_chatapp_client" className='href' target="_blank">Github Repository (client)</a>
             </div>
