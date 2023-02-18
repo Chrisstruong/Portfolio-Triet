982f3bee797a064d67088639a216bcdec8f36d1d
chrisstruong <chrisstruong@gmail.com>
2023-02-17 19:05:26 -0500

    update live link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Spline from '@splinetool/react-spline';
 import { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import Project3Details from './components/Project3Details';
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { HiOutlineMail } from "react-icons/hi";
@@ -13,6 +14,7 @@ import project22 from './gif/project22.png'
 import capstone from './gif/Capstone.gif'
 import Capstone from './gif/Capstone.png'
 import CapstoneDetail from './components/CapstoneDetail'
+import { IoIosLink } from "react-icons/io";
 
 
 function App() {
@@ -20,6 +22,9 @@ function App() {
   const [visibleDetailCapstone, setVisibleDetailCapstone] = useState(false)
   const [visibleDetails3, setVisibleDetails3] = useState(false)
   const [visibleDetails2, setVisibleDetails2] = useState(false)
+  const [previewLink, setPreviewLink] = useState(false)
+  const [previewLink2, setPreviewLink2] = useState(false)
+  const [previewLink3, setPreviewLink3] = useState(false)
 
 
   const handClickGOrA = () => {
@@ -35,6 +40,15 @@ function App() {
     setVisibleDetails2(!visibleDetails2)
   }
 
+  const preview = () => {
+    setPreviewLink(!previewLink)
+  }
+  const preview2 = () => {
+    setPreviewLink2(!previewLink2)
+  }
+  const preview3 = () => {
+    setPreviewLink3(!previewLink3)
+  }
 
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
   const windowSizeCheck = windowSize.current[0] >= 500
@@ -46,9 +60,9 @@ function App() {
             {/* <Spline scene="https://prod.spline.design/YzuLLwr1Dp3RmGw1/scene.splinecode"/> */}
             {/* <Spline scene="https://my.spline.design/gradient-ec5a516a7797a2defedc0d0ffeac09f2/"/> */}
             {windowSizeCheck
-            ? <iframe src='https://my.spline.design/gradient-ec5a516a7797a2defedc0d0ffeac09f2/' frameborder='0' width='100%' height='100%'></iframe>
-            : <iframe src='https://my.spline.design/gradient12promax-e041ce601a2d932ea3228deddcaf2ba7/' frameborder='0' width='100%' height='100%'></iframe>}
-            
+              ? <iframe src='https://my.spline.design/gradient-ec5a516a7797a2defedc0d0ffeac09f2/' frameborder='0' width='100%' height='100%'></iframe>
+              : <iframe src='https://my.spline.design/gradient12promax-e041ce601a2d932ea3228deddcaf2ba7/' frameborder='0' width='100%' height='100%'></iframe>}
+
           </div>
         </div>
       </div>
@@ -58,7 +72,7 @@ function App() {
           <h1 className='title'>Triet Truong - <br /> Software Engineer</h1>
           <p id='brand-statement'>I am a solutions-driven software engineer with a sharp, analytical mind who is highly motivated by challenge. My aptitude for digesting and implementing new technologies makes me both efficient when given a clear task, and exceptionally versatile when faced with ambiguity or road blocks.</p>
           <div className='second-main-part'>
-            <h2 className='recent-work'>Recent work <span className='click-for-details'>(click for details)</span></h2>
+            <h2 className='recent-work'>Recent work <span className='click-for-details'>(click title for live link)</span></h2>
 
             <div className='project3-div'>
               <div className='project3'>
@@ -66,7 +80,18 @@ function App() {
                 <img src={capstone} alt="project3" className='project3-giphy' />
               </div>
               <div className='decoration-container'>
-                <h2 className='project-title'>SEI1031_GRADUATES</h2>
+
+                <a href="https://sei-1031.netlify.app/" target="_blank" className='project-title' onMouseEnter={()=>setPreviewLink(true)} onMouseLeave={()=>setPreviewLink(false)}>SEI1031_GRADUATES</a>
+                {previewLink ?
+                  <div className='preview-container'>
+                    <div className='link-container'>
+                      <IoIosLink />
+                      <a href="https://sei-1031.netlify.app/" target="_blank" onClick={() => setPreviewLink(false)}>Go to: https://sei-1031.netlify.app/</a>
+                    </div>
+                    <img src={Capstone} className='project3-image-preview' alt="" />
+                  </div>
+                  : ""}
+
                 <p className='project-description'>SEI1031_GRADUATES is a chat app that allows SEI1031 cohort to keep in touch after graduation <span onClick={handleClickCapstone} className='additional-details'>{visibleDetailCapstone ? "Show Less" : "Show More"}</span>
                 </p>
                 {visibleDetailCapstone ? <CapstoneDetail /> : ""}
@@ -80,7 +105,18 @@ function App() {
                 <img src={Project3Giphy} alt="project3" className='project3-giphy' />
               </div>
               <div className='decoration-container'>
-                <h2 className='project-title'>MOVIEBUFF</h2>
+
+                <a href="https://movie-buff-movie.netlify.app/" target="_blank" className='project-title' onMouseEnter={()=>setPreviewLink2(true)} onMouseLeave={()=>setPreviewLink2(false)}>MOVIEBUFF</a>
+                {previewLink2 ?
+                  <div className='preview-container'>
+                    <div className='link-container2'>
+                      <IoIosLink />
+                      <a href="https://movie-buff-movie.netlify.app/" target="_blank" onClick={() => setPreviewLink2(false)}>Go to: https://movie-buff-movie.netlify.app/</a>
+                    </div>
+                    <img src={Project3Image} className='project3-image-preview2' alt="" />
+                  </div>
+                  : ""}
+
                 <p className='project-description'>MovieBuff is a web app that allows users to rate and review the most action-packed and sweaty movies from the 80's and 90's <span onClick={handleClick3} className='additional-details'>{visibleDetails3 ? "Show Less" : "Show More"}</span>
                 </p>
                 {visibleDetails3 ? <Project3Details /> : ""}
@@ -94,7 +130,18 @@ function App() {
                 <img src={Project2Giphy} alt="project3" className='project3-giphy' />
               </div>
               <div className='decoration-container'>
-                <h2 className='project-title'>BREED_BEAUTY</h2>
+
+                <a href="https://breed-beauty.netlify.app/" target="_blank" className='project-title' onMouseEnter={()=>setPreviewLink3(true)} onMouseLeave={()=>setPreviewLink3(false)}>BREED_BEAUTY</a>
+                {previewLink3 ?
+                  <div className='preview-container'>
+                    <div className='link-container3'>
+                      <IoIosLink />
+                      <a href="https://breed-beauty.netlify.app/" target="_blank" onClick={() => setPreviewLink3(false)}>Go to: https://breed-beauty.netlify.app/</a>
+                    </div>
+                    <img src={project22} className='project3-image-preview3' alt="" />
+                  </div>
+                  : ""}
+
                 <p className='project-description'>Sometimes people just want to see photos of cute dogs. Zach and Triet app's mission is to solve the problem of by allowing the user to randomly generate a photo of a dog from a group of over 1000 dog photos.<span onClick={handleClick2} className='additional-details'>{visibleDetails2 ? "Show Less" : "Show More"}</span>
                 </p>
                 {visibleDetails2 ? <Project2Details /> : ""}
--- src/components/Project2Details.jsx
+++ src/components/Project2Details.jsx
@@ -4,7 +4,7 @@ function Project2Details (props) {
         <div className='projectDetails'>
             <p>TECH: React.js | JavaScript | CSS3 | HTML5 | Flexbox | EJS | JSX</p>
             <div className="projects-href">
-            <a href="https://breed-beauty.netlify.app/" className='href' target="_blank">Live Link</a>
+            {/* <a href="https://breed-beauty.netlify.app/" className='href' target="_blank">Live Link</a> */}
             <a href="https://github.com/Chrisstruong/BreedBeauty" className='href' target="_blank">Github Repository</a>
             </div>
         </div>
